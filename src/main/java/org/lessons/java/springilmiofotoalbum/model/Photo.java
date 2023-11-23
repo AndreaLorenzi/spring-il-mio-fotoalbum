c97afad14079161467cadddd8e75e3c1c0603223
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-11-23 10:23:46 +0100

    add model photo

--- src/main/java/org/lessons/java/springilmiofotoalbum/model/Photo.java
+++ src/main/java/org/lessons/java/springilmiofotoalbum/model/Photo.java
@@ -1,4 +1,63 @@
 package org.lessons.java.springilmiofotoalbum.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "photos")
 public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Title must be not blank")
+    @Size(max = 255, message = "lenght must be less than 255")
+    private String title;
+    @NotBlank(message = "Description must be not blank")
+    @Size(max = 255, message = "lenght must be less than 255")
+    private String description;
+    @NotBlank(message = "imageUrl must be not blank")
+    @Size(max = 255, message = "lenght must be less than 255")
+    private String url;
+    private boolean visible;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 }
